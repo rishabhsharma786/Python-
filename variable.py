74024a6d65b3c6419e8ff292f677b72657584a8a
Rishabh Sharma <43056968+rishabhsharma786@users.noreply.github.com>
2019-07-13 14:44:33 +0530

    #using and show absolute value, hexadecimal, octal, log, pow ,sqrt, pi, and trigonometric

---
+++ variable.py
@@ -0,0 +1,35 @@
+#using and show absolute value, hexadecimal, octal, log, pow ,sqrt, pi, and trigonometric
+x=10
+print(x)
+
+y=25.8
+print(y)
+
+z=0xAE3  #hexadecimal
+print(z)
+
+p=0o711 #octal
+print(p)
+
+x=-45
+print(abs(x))   # abs ->absolute value
+
+x=25.8
+print(math.ceil(x))
+print(math.floor(x))
+print(math.log(5))
+print(math.pow(2,3))
+print(math.sqrt(25))
+a,b,c,d=25,35,110,3
+print(max(a,b,c,d))
+print(min(a,b,c,d))
+
+print(math.pi)
+print(math.sin(45))
+print(math.cos(45))
+print(math.tan(45))
+
+
+
+
+
